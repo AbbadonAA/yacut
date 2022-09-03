5cbffb23362c3c45142925caf076bab2d32c9f1c
Pushkarev Anton <pushkarevantona@gmail.com>
2022-09-03 19:52:51 +0300

    comments deleted

--- settings.py
+++ settings.py
@@ -5,7 +5,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', default='qwerty12345')
-    # Не проходит pytest, хотя конфиг SORT_KEYS позволяет сделать
-    # вывод в порядке, указанном в спецификации. Без него url и
-    # short_link выводятся наоборот: short_link, url
     # JSON_SORT_KEYS = False
