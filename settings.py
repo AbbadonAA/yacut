4bdc9680e00e7613ebd4feeee31555880d03d4b2
Pushkarev Anton <pushkarevantona@gmail.com>
2022-09-03 03:12:52 +0300

    default in settings for pytest

--- settings.py
+++ settings.py
@@ -2,9 +2,9 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', default='qwerty12345')
     # Не проходит pytest, хотя конфиг SORT_KEYS позволяет сделать
     # вывод в порядке, указанном в спецификации. Без него url и
     # short_link выводятся наоборот: short_link, url
